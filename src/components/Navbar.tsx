0c5e8199b5084f1598b6fd19328a37b267921bad
soenkeba <sbartinfo@gmx.de>
2025-12-13 20:23:12 +0100

    Fix: Remove unused Link import from Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { motion, useScroll, useMotionValueEvent } from 'framer-motion'
 
 const Navbar = () => {
