0b3c1959928d949903b5f6b1e485d3de5a784386
soenkeba <sbartinfo@gmx.de>
2025-12-13 19:59:28 +0100

    Fix TypeScript build errors: remove unused imports and parameters

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -71,7 +71,7 @@ const ImageCarousel = () => {
             drag="x"
             dragConstraints={{ left: 0, right: 0 }}
             dragElastic={1}
-            onDragEnd={(e, { offset, velocity }) => {
+            onDragEnd={(_e, { offset, velocity }) => {
               const swipe = swipePower(offset.x, velocity.x)
 
               if (swipe < -swipeConfidenceThreshold) {
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { motion, useScroll, useMotionValueEvent } from 'framer-motion'
 
 const Navbar = () => {
